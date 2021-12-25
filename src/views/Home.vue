8717b9307dabd4e96b2d62c5333cde3a03455bfa
石川恵実里 <emiri@emirinoMacBook-Pro.local>
2021-12-25 14:39:24 +0900

    my-first-commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <div>test</div>
+  <!-- <hello-world /> -->
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import { Component, Vue } from "vue-property-decorator";
+// import HelloWorld from "../components/HelloWorld.vue";
 @Component({
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 })
 export default class Home extends Vue {}
