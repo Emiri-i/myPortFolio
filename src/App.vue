f39e2573a84392e708ad0d3dda1d50079bd953d2
石川恵実里 <emiri@emirinoMacBook-Pro.local>
2021-12-26 18:38:44 +0900

    created Header

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,28 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-app-bar app color="#BFA2DB" dark>
+      <v-toolbar-title>Emiri's Portfolio</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn icon @click="goToLinkedIn">
+        <v-icon>mdi-linkedin</v-icon>
       </v-btn>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
     </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary right>
+      <v-list nav dense>
+        <v-list-item-group v-model="navBarGroup" active-class="header-active">
+          <v-list-item v-for="item in navItems" :key="item.id">
+            <v-list-item-icon class="nav-item">
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title class="nav-item">
+              {{ item.title }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <router-view />
@@ -40,7 +31,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Watch } from "vue-property-decorator";
 import Home from "./views/Home.vue";
 
 @Component({
@@ -48,20 +39,41 @@ import Home from "./views/Home.vue";
     Home,
   },
 })
-export default class App extends Vue {}
-// import Vue from 'vue';
+export default class App extends Vue {
+  drawer: boolean = false;
+  navBarGroup: number = -1;
+  linkedinURL: string = "https://www.linkedin.com/in/emiri-ishikawa-5579341b0/";
+  navItems: any[] = [
+    { id: 1, title: "Home", icon: "mdi-home" },
+    { id: 2, title: "About Me", icon: "mdi-account" },
+    { id: 3, title: "Skills", icon: "mdi-keyboard" },
+    { id: 4, title: "Projects", icon: "mdi-file-multiple" },
+  ];
 
-// export default Vue.extend({
-//   name: 'App',
+  @Watch("navBarGroup")
+  test() {
+    console.log("navBarGroup", this.navBarGroup);
+  }
 
-//   data: () => ({
-//     //
-//   }),
-// });
+  goToLinkedIn() {
+    const win = window.open(this.linkedinURL);
+    win?.focus();
+  }
+}
 </script>
 
 <style lang="scss">
 .v-main {
   background-color: #f4f3f3;
+}
+.header-active {
+  background-color: #f0d9ff;
+  > .v-list-item__title,
+  .v-list-item__icon {
+    color: #aa14f0 !important;
+  }
+}
+.nav-item {
+  color: #757575;
 }
 </style>
