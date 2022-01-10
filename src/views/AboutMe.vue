0cd4c991b46618cd25300904c7e535acb7619b06
石川恵実里 <emiri@emirinoMacBook-Pro.local>
2022-01-10 18:37:49 +0900

    created aboutme page

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,6 +1,44 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+      <v-container>
+        <h1
+          class="text-center"
+          style="font-family: cursive; color: #bfa2db; font-size: 2.75em"
+        >
+          - About me -
+        </h1>
+        <v-row class="mt-3">
+          <v-col cols="12" sm="3" class="d-flex align-center justify-center">
+            <v-img src="../libs/F.jpg" style="max-width: 200px"></v-img>
+          </v-col>
+          <v-col cols="12" sm="9">
+            <div class="pl-6 about-me-sentence" style="font-size: 1.25em">
+              <div style="font-size: 2em" class="typing">
+                Hi, I'm Emiri.
+                <!-- <strong class="typing">Hi, I'm Emiri.</strong> -->
+              </div>
+              <div>a Frontend-developer from Japan.</div>
+              <div class="mt-4">
+                I have 3 years developer experience in total, including
+                Salesforce developer and current Frotnend developer role.
+              </div>
+              <div class="mt-4">
+                Not only Frontend-Develoeper, but also currently in charge of UI
+                design. I enjoy having responsibilities for both creating UI
+                that I envision and giving them shape by coding.
+              </div>
+              <div class="mt-4">
+                I am motivated, open-minded, a hard worker, and have the ability
+                to work cross-functionally. Besides that, I also have experience
+                working internationally, so I can easily adjust myself to new
+                environments and love new challenges.
+              </div>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
   </div>
 </template>
 
@@ -11,3 +49,35 @@ import { Component, Vue } from "vue-property-decorator";
 })
 export default class AboutMe extends Vue {}
 </script>
+
+<style lang="scss">
+.v-image {
+  box-shadow: 20px 20px 0 #f0d9ff;
+  margin-bottom: 20px; //centering
+  margin-right: 20px; //centering
+}
+.about-me-sentence {
+  color: #7f7c82;
+}
+.typing {
+  display: inline-block;
+  width: 10ch;
+  overflow: hidden;
+  white-space: nowrap;
+  white-space: nowrap;
+  overflow: hidden;
+  border-right: 3px solid;
+  animation: typing 2s steps(14), blink 1.2s infinite;
+}
+@keyframes typing {
+  from {
+    width: 0; // 行頭から開始
+  }
+}
+
+@keyframes blink {
+  50% {
+    border-color: transparent; // チカチカする表現部分
+  }
+}
+</style>
